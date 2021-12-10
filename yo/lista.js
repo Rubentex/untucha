53df524ac384f78539205fe6982b25123fde7aeb
Rubentex <77239189+Rubentex@users.noreply.github.com>
2021-12-09 20:55:41 -0400

    s

---
+++ yo/lista.js
@@ -0,0 +1,124 @@
+/* PLAYLIST  */
+var myPlaylist = [
+  {
+    type: "youtube",
+    title: "Curso Completo Photoshop 2021",
+    author: "Yoney Gallardo",
+    sources: [{ 
+      src: "mzSr6rXd9lc", 
+      type: "youtube"
+    }],
+    src: "mzSr6rXd9lc",
+    poster: "https://img.youtube.com/vi/mzSr6rXd9lc/hqdefault.jpg"
+  },
+  {
+    type: "youtube",
+    title: "Avicii - SOS ft. Aloe Blacc (Unofficial Video)",
+    author: "Avicii",
+    sources: [{ 
+      src: "RnVbU3NYrZw", 
+      type: "youtube"
+    }],
+    poster: "https://i.ytimg.com/vi/RnVbU3NYrZw/default.jpg"
+  },
+  {
+    type: "youtube",
+    title: "Loud Luxury feat. brando - Body (Official Video HD)",
+    author: "Loud Luxury",
+    sources: [{ 
+      src: "https://www.youtube.com/watch?v=UyxKu20xmBs", 
+      type: "youtube"
+    }],
+    poster: "https://i.ytimg.com/vi/UyxKu20xmBs/default.jpg"
+  },
+  {
+    type: "youtube",
+    title: "Loud Luxury x anders - Love No More (Official Music Video)",
+    author: "Loud Luxury",
+    sources: [{ 
+      src: "https://www.youtube.com/watch?v=PJF0SBwfDq8", 
+      type: "youtube"
+    }],
+    poster: "https://img.youtube.com/vi/PJF0SBwfDq8/hqdefault.jpg"
+  },
+  {
+    type: "audio",
+    title: "Clublife by Tiësto 542 podcast ",
+    author: "Tiësto",
+    sources: [{ 
+      src: "http://feed.pippa.io/public/streams/593eded1acfa040562f3480b/episodes/59c0c870ed6a93163c0a193d.m4a", 
+      type: "m4v"
+    }],
+    poster: "https://img.youtube.com/vi/r6KXy0j85AM/hqdefault.jpg"
+  },
+  {
+    type: "audio",
+    title: "Vocal Trance Vol 261",
+    author: "Sonnydeejay",
+    sources: [{ 
+      src: "http://archive.org/download/SonnydeejayVocalTranceVol261/Sonnydeejay%20-Vocal%20Trance%20vol%20261.mp3", 
+      type: "mp3"
+    }],
+    poster: "http://4.bp.blogspot.com/-d6IPBUIj6YE/ThpRaIGJXtI/AAAAAAAABQ8/54RNlCrKCv4/s1600/podcast.jpg"
+  },
+  {
+    type: "youtube",
+    title: "2 hours Trance Music - Armin Van Buuren",
+    author: "Armin van Buuren",
+    sources: [{ 
+      src: "https://www.youtube.com/watch?v=r6KXy0j85AM", 
+      type: "youtube"
+    }],
+    poster: "https://img.youtube.com/vi/r6KXy0j85AM/hqdefault.jpg"
+  },
+  {
+    type: "youtube",
+    title: "2 hours Trance Music - Armin Van Buuren",
+    author: "Armin van Buuren",
+    sources: [{ 
+      src: "https://www.youtube.com/watch?v=r6KXy0j85AM", 
+      type: "youtube"
+    }],
+    poster: "https://img.youtube.com/vi/r6KXy0j85AM/hqdefault.jpg"
+  },
+  {
+    type: "youtube",
+    title: "2 hours Trance Music - Armin Van Buuren",
+    author: "Armin van Buuren",
+    sources: [{ 
+      src: "https://www.youtube.com/watch?v=r6KXy0j85AM", 
+      type: "youtube"
+    }],
+    poster: "https://img.youtube.com/vi/r6KXy0j85AM/hqdefault.jpg"
+  },
+  {
+    type: "youtube",
+    title: "2 hours Trance Music - Armin Van Buuren",
+    author: "Armin van Buuren",
+    sources: [{ 
+      src: "https://www.youtube.com/watch?v=r6KXy0j85AM", 
+      type: "youtube"
+    }],
+    poster: "https://img.youtube.com/vi/r6KXy0j85AM/hqdefault.jpg"
+  },
+  {
+    type: "youtube",
+    title: "2 hours Trance Music - Armin Van Buuren",
+    author: "Armin van Buuren",
+    sources: [{ 
+      src: "https://www.youtube.com/watch?v=r6KXy0j85AM", 
+      type: "youtube"
+    }],
+    poster: "https://img.youtube.com/vi/r6KXy0j85AM/hqdefault.jpg"
+  },
+  {
+    type: "youtube",
+    title: "2 hours Trance Music - Armin Van Buuren",
+    author: "Armin van Buuren",
+    sources: [{ 
+      src: "https://www.youtube.com/watch?v=r6KXy0j85AM", 
+      type: "youtube"
+    }],
+    poster: "https://img.youtube.com/vi/r6KXy0j85AM/hqdefault.jpg"
+  }
+];
